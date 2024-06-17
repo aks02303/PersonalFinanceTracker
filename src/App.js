8789e78075ae933543b1be569b459552993a15f9
aks02303 <akshaypandey0302@gmail.com>
2024-06-17 14:20:48 +0530

    first comit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import SignUpSignIn from "./components/Signup";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
